c2e1b5cb44d18db7a89fbc3e206629b88bcd8f28
Nikita Donskoy <donskoynik1995@gmail.com>
2023-09-08 20:59:24 +0300

    add chart js

--- src/modules/statistic/pages/HelperStatistic/index.jsx
+++ src/modules/statistic/pages/HelperStatistic/index.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import AppLayout from "../../../common/components/AppLayout";
+import { Chart, registerables } from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+
 
 const ProductsListPage = () => {
+    Chart.register(...registerables);
+    const data= {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      }
+      const  options= {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+    }
     return (
         <AppLayout>
-           <div>helper statistic</div>
+        <Bar
+      data={data}
+      options={options}
+        />
+
         </AppLayout>
     );
 };
