bf7f8e5fb3f44f4813d323750e94da025e226b0a
Nikita Donskoy <donskoynik1995@gmail.com>
2023-09-08 23:00:15 +0300

    pritter

--- src/modules/statistic/pages/HelperStatistic/index.jsx
+++ src/modules/statistic/pages/HelperStatistic/index.jsx
@@ -1,35 +1,15 @@
-import React, {useEffect} from "react";
-import AppLayout from "../../../common/components/AppLayout";
-import {useUserData} from "../../../auth/context/UserDataContext";
-import Plot from 'react-plotly.js';
-
+import React, { useEffect } from 'react';
+import AppLayout from '../../../common/components/AppLayout';
+import { useUserData } from '../../../auth/context/UserDataContext';
+// import { SINGLE_PRODUCT, SET_PRODUCTS } from
 const ProductsListPage = () => {
   const { usersData, getAllUsersDataInfo } = useUserData();
-  
+
   useEffect(() => {
     getAllUsersDataInfo();
   }, []);
 
-  console.log('usersData', usersData);
-
-    return (
-        <AppLayout>
-      
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
-        </AppLayout>
-    );
+  return <AppLayout></AppLayout>;
 };
 
 export default ProductsListPage;
