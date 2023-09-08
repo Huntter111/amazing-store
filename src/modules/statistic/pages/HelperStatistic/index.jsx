cb941f7938d3d22321413924bb625641aeb2ea23
Nikita Donskoy <donskoynik1995@gmail.com>
2023-09-08 22:49:25 +0300

    add plotly js

--- src/modules/statistic/pages/HelperStatistic/index.jsx
+++ src/modules/statistic/pages/HelperStatistic/index.jsx
@@ -1,43 +1,33 @@
 import React, {useEffect} from "react";
 import AppLayout from "../../../common/components/AppLayout";
-import { Chart, registerables } from 'chart.js';
-
-import { Bar } from 'react-chartjs-2';
 import {useUserData} from "../../../auth/context/UserDataContext";
-
+import Plot from 'react-plotly.js';
 
 const ProductsListPage = () => {
   const { usersData, getAllUsersDataInfo } = useUserData();
-
+  
   useEffect(() => {
     getAllUsersDataInfo();
   }, []);
 
   console.log('usersData', usersData);
 
-    Chart.register(...registerables);
-    const data= {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      }
-      const  options= {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-    }
     return (
         <AppLayout>
-        <Bar
-      data={data}
-      options={options}
-        />
-
+      
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+      />
         </AppLayout>
     );
 };
