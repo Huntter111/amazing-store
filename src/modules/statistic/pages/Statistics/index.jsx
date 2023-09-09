a68343ca6a7adb8704b2accadce04ad1c4497fb0
Nikita Donskoy <donskoynik1995@gmail.com>
2023-09-09 19:07:38 +0300

    created butonst in statistic page

--- src/modules/statistic/pages/Statistics/index.jsx
+++ src/modules/statistic/pages/Statistics/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import AppLayout from '../../../common/components/AppLayout';
 import { Link } from 'react-router-dom';
+import { ROUTES } from '../../../../routes/';
+import productImg from '../../../../assets/product.png';
+import helperImg from '../../../../assets/helper.png';
+import styles from './statisticsPage.module.scss';
 
 const StatisticsPage = () => {
   return (
     <AppLayout>
-      <button>
-        <Link to={'/helper-statistic'}>Helper</Link>
-      </button>
-      <button>
-        <Link to={'/product-statistic'}>product</Link>
-      </button>
+      <nav>
+        <Link to={ROUTES.HELPER_STATISTIC} className={styles.linkButtons}>
+          <div className={styles.linkContent}>
+            <h1>Helper</h1>
+            <img alt="helper img" src={helperImg} />
+          </div>
+        </Link>
+
+        <Link to={ROUTES.PRODUCT_STATISTIC} className={styles.linkButtons}>
+          <div className={styles.linkContent}>
+            <h1>Products</h1>
+            <img alt="helper img" src={productImg} />
+          </div>
+        </Link>
+      </nav>
     </AppLayout>
   );
 };
