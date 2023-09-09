03ab9d90ddb80cd4fe3f0126de9e1e3cbffc4278
Mihail Donskoy <mihail.donskoy@jointoit.com>
2023-09-10 02:34:35 +0300

    add get products statistic data fn

--- src/modules/statistic/helpers/getProductsStatisticData.js
+++ src/modules/statistic/helpers/getProductsStatisticData.js
@@ -1,4 +1,6 @@
-
+import moment from 'moment';
+import {TYPE_CHART} from "../../common/constants";
+import {PRODUCT_TYPES} from "../../products/constants";
 const formattedProductsList = (data) => {
   return data.reduce((acc, item) => {
     acc[item.id] = item.name;
@@ -16,12 +18,69 @@ const formattedOrdersList = (data) => {
   }, []);
 };
 
+const getFilteredOrdersData = (ordersList, from, to, type) => {
+  if(from && to) {
+    ordersList = ordersList.filter(item => {
+      return !moment(item.orderDate).isBefore(from, 'date') && !moment(item.orderDate).isAfter(to, 'date')
+    })
+  }
 
-const getProductsStatisticData = (products, orders) => {
+  if(type) {
+    ordersList = ordersList.filter(item => item.type === type);
+  }
+
+  return ordersList;
+}
+const generateDataByDataType = (ordersList, productsList, dataType, subDataType) => {
+  let collectedData;
+
+  if(dataType && subDataType) {
+    collectedData = ordersList.reduce((acc, item) => {
+      if (acc[productsList[item.id]]) {
+        acc[productsList[item.id]] = acc[productsList[item.id]] + item[dataType][subDataType];
+      } else {
+        acc[productsList[item.id]] = item[dataType][subDataType];
+      }
+      return acc;
+    }, {});
+  } else {
+    collectedData =  ordersList.reduce((acc, item) => {
+      if (acc[productsList[item.id]]) {
+        acc[productsList[item.id]] = acc[productsList[item.id]] + item[dataType];
+      } else {
+        acc[productsList[item.id]] = item[dataType];
+      }
+      return acc;
+    }, {});
+  }
+
+  return {
+    x: Object.keys(collectedData),
+    y: Object.values(collectedData),
+  }
+};
+const getProductsStatisticData = (products, orders, from, to, type) => {
   const productsList = products && formattedProductsList(products);
   const ordersList = orders && formattedOrdersList(orders);
+  const filteredOrdersList = orders && getFilteredOrdersData(ordersList, productsList, from, to, type);
 
-  console.log('ordersList', ordersList)
+  const productsByCount = generateDataByDataType(filteredOrdersList, productsList, 'count');
+  const productsByPrice = generateDataByDataType(filteredOrdersList, productsList, 'price', 'priceAmount');
+
+  return [
+    {
+      graphType: TYPE_CHART.PIE,
+      title: `Продаж продуктів за категорією ${PRODUCT_TYPES[type]} (кількість)`,
+      data: productsByCount,
+      hole: 0.5,
+    },
+    {
+      graphType: TYPE_CHART.PIE,
+      title: `Продаж продуктів за категорією ${PRODUCT_TYPES[type]} (виручка грн)`,
+      data: productsByPrice,
+      hole: 0.5,
+    }
+  ]
 }
 
 export default getProductsStatisticData;
