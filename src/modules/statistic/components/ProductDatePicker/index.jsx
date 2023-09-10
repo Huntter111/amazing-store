1ae41c6df92a1184fa51865040d1f63b8262170a
Mihail Donskoy <mihail.donskoy@jointoit.com>
2023-09-10 19:37:39 +0300

    refactoring

--- src/modules/statistic/components/ProductDatePicker/index.jsx
+++ src/modules/statistic/components/ProductDatePicker/index.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react';
+import {useCallback, useMemo, useState} from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
 const ProductDatePicker = ({ setDateRange, highlightDates }) => {
   const [range, setRange] = useState([null, null]);
-  const [startDate, endDate] = range;
 
-  const hendlerChange = (newData) => {
+  const hendlerChange = useCallback((newData) => {
     const [startDate, endDate] = newData;
     const isStart = startDate ? moment(startDate) : null;
     const isEnd = endDate ? moment(endDate) : null;
 
     setDateRange({ from: isStart, to: isEnd });
     setRange(newData);
-  };
+    // eslint-disable-next-line
+  },[]);
 
   const days = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'];
   const months = [
@@ -40,10 +40,11 @@ const ProductDatePicker = ({ setDateRange, highlightDates }) => {
       date: () => 'mm/dd/yyyy',
     },
   };
-  const isHighlightDates = highlightDates ? highlightDates : [];
 
-  return (
-    <DatePicker
+  return useMemo(() => {
+    const [startDate, endDate] = range;
+    const isHighlightDates = highlightDates || [];
+    return (<DatePicker
       highlightDates={isHighlightDates}
       showIcon
       placeholderText="Виберiть дату"
@@ -57,8 +58,9 @@ const ProductDatePicker = ({ setDateRange, highlightDates }) => {
       }}
       isClearable={true}
       maxDate={moment().toDate()}
-    />
-  );
+    />)
+    // eslint-disable-next-line
+  }, [range, highlightDates]);
 };
 
 export default ProductDatePicker;
