2106e2cd242ae1cbaa22bddad79517ef07701638
Mihail Donskoy <mihail.donskoy@jointoit.com>
2023-09-25 19:03:00 +0300

    change name of tables

--- src/modules/statistic/components/ProductsGroups/index.jsx
+++ src/modules/statistic/components/ProductsGroups/index.jsx
@@ -76,19 +76,19 @@ const ProductsGroups = ({orders}) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.tableWrapper}>
-        <h3 className={styles.groupATitle}><b>Група А</b> (Високорейтингові продукти групи ABC)</h3>
+        <h3 className={styles.groupATitle}><b>Група продуктів з найвищим показником продажів</b></h3>
         <Table dataSource={productsGroups?.groupA} columns={columns} pagination={false} />
       </div>
       <div className={styles.tableWrapper}>
-        <h3 className={styles.groupBCTitle}><b>Група B</b> (Високорейтингові продукти групи ABC)</h3>
+        <h3 className={styles.groupBCTitle}><b>Група продуктів з високим показником продажів</b></h3>
         <Table dataSource={productsGroups?.groupB} columns={columns} pagination={false} />
       </div>
       <div className={styles.tableWrapper}>
-        <h3 className={styles.groupBCTitle}><b>Група C</b> (Високорейтингові продукти групи ABC)</h3>
+        <h3 className={styles.groupBCTitle}><b>Група продуктів з середнім показником продажів</b></h3>
         <Table dataSource={productsGroups?.groupC} columns={columns} pagination={false} />
       </div>
       <div className={styles.tableWrapper}>
-        <h3 className={styles.groupXTitle}><b>Група X</b> (Низькорейтингові продукти)</h3>
+        <h3 className={styles.groupXTitle}><b>Група продуктів з низьким показником продажів</b></h3>
         <Table dataSource={productsGroups?.groupX} columns={columns} pagination={false} />
       </div>
     </div>
